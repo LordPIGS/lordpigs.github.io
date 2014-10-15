400ae6e913cbc9444bed9c3af4b58793730b5226
Daniel Escoz <darkhogg@gmail.com>
2014-10-15 11:42:41 +0200

    Lots of changes that I should not be committing together but still

--- js/character-quotes.js
+++ js/character-quotes.js
@@ -1,4 +1,13 @@
+//+ Jonas Raoni Soares Silva
+//@ http://jsfromhell.com/array/shuffle [v1.0]
+function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
 $(function () {
+    var intervalTime = 1200;
+    var animTime = 500;
     var classBase = 'character-quote';
     var classHidden = 'character-quote-hidden';
     var classes = [
@@ -6,34 +15,38 @@ $(function () {
         'character-quote-third', 'character-quote-fourth'
     ];
 
-    var quotes = $('.' + classBase).toArray();
-    quotes.forEach(function (rawelem) {
-        var elem = $(rawelem);
-        classes.forEach(function (cls) {
-            elem.removeClass(cls);
-        });
-        elem.addClass(classHidden);
+    /* Take all elements */
+    var elemsLeft = shuffle($('.' + classBase)
+        .removeClass(classes.join(' '))
+        .addClass(classHidden)
+        .toArray()
+    );
+
+    /* Select and remove the first ones */
+    classes.forEach(function (cls) {
+        $(elemsLeft[0]).removeClass(classHidden).addClass(cls);
+        elemsLeft.shift();
     });
-    for (i in classes) {
-        $(quotes[i]).addClass(classes[i]).removeClass(classHidden);
-    }
 
-    var animationTime = 350;
-    var changeInterval = setInterval(function () {
-        // Select one that is not visible randomly
-        var hiddenQuotes = $('.' + classHidden);
-        var target = $(hiddenQuotes.get(Math.floor(Math.random() * hiddenQuotes.length)));
+    var nextIndex = 0;
+    var intervalFunc = function () {
+        /* If there are no more elements left, get some more */
+        if (elemsLeft.length == 0) {
+            elemsLeft = shuffle($('.' + classBase + '.' + classHidden).toArray());
+            console.log(elemsLeft);
+        }
 
-        // Select the position also randomly
-        var positionClass = classes[Math.floor(Math.random() * classes.length)];
-        var oldTarget = $('.' + positionClass)
-
-        // Remove the old one, set the new one
-        oldTarget.css({'z-index': 100}).animate({
-            'opacity': 0
-        }, function () {
-            oldTarget.css({'opacity': 1, 'z-index': 0}).removeClass(positionClass).addClass(classHidden);
+        /* Take the first from the list and change it for the next class */
+        var nextClass = classes[nextIndex];
+        $('.' + nextClass).css({'z-index': 100}).animate({'opacity': 0}, animTime, function () {
+            $(this).removeClass(nextClass).addClass(classHidden).css({'opacity': 1, 'z-index': 0});
         });
-        target.removeClass(classHidden).addClass(positionClass);
-    }, animationTime * 3);
+        $(elemsLeft[0]).removeClass(classHidden).addClass(nextClass);
+
+        /* Prepare the next iteration */
+        nextIndex = (nextIndex + 1) % classes.length;
+        elemsLeft.shift();
+    };
+
+    setInterval(intervalFunc, intervalTime);
 });
